68e97bb77d70156239dd9db73269c1efe405bcd3
saiful8183 <Manha103019@gmail.com>
2023-03-04 11:49:26 -0500

    Added one new class and made some changes to other class

---
+++ JavaFeb23/src/ActionItems/AddingNUmbers.java
@@ -0,0 +1,41 @@
+package ActionItems;
+
+public class AddingNUmbers {
+
+	public static void main(String[] args) {
+
+
+		/*
+		 * int sum=0;
+		 *  for(int i=0;i<=15;i++) { 
+		 *  sum=sum+ i; 
+		 *  }
+		 * System.out.println("Sum is :"+ sum);
+		 */
+
+		
+		
+		
+		
+		
+		
+		int sum1=0;
+		int i=0;
+		while(i<=15)
+		{
+			
+		sum1=sum1+i;
+		i++;
+			
+		}
+		System.out.println("Sum is :"+ sum1);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ JavaFeb23/src/array/Array_Demo.java
@@ -0,0 +1,50 @@
+package array;
+
+public class Array_Demo {
+
+	public static void main(String[] args) {
+		
+		//Declaring array
+		String[] myArray=new String[3];
+		
+		
+		
+		//Assaigning value to the array
+		//Assaigning value to the array
+		myArray[0]="Selenium";
+		myArray[1]="Eclipse";
+		myArray[2]="java";
+		
+		System.out.println(myArray[1]);
+		
+		for(int i=0;i<myArray.length;i++){
+		System.out.println(myArray[i]);
+		
+		
+		}	
+		
+		//Another way of declaring the array
+		
+		int []number= {4,2,7};
+		
+		for(int i=0;i<number.length;i++){
+			System.out.println(myArray[i]);
+		
+		
+		}
+		
+		System.out.println("Exemple of enhanced for loop");
+		//Enhanced for loop
+		
+		
+		for(int v:number) {
+			System.out.println(v);	
+			
+			
+			
+		}
+		
+
+	}
+
+}
